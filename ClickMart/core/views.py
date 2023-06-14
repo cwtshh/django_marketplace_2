366bc985ad358a1d456664b473acc64cafd09992
cwtshh <gucosta1719@gmail.com>
2023-06-14 17:26:22 -0300

    adding register, login and logout

--- ClickMart/core/views.py
+++ ClickMart/core/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as logouts
 
 # importa os modelos
 from item.models import Category, Item
+
+from .forms import SignUpForm
 
 
 def index(request):
@@ -16,3 +19,26 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def logout(request):
+    if request.method == "POST":
+        logouts(request)
+        return redirect('/')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
+
+
